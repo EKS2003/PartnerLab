8c27fa0ce5d2f8d4ff2d93a7c6816859a94a2ed6
EKS2003 <ekrivoysekler@ufl.edu>
2023-10-25 15:44:15 -0400

    Encoder Menu

--- Print.py
+++ Print.py
@@ -1,2 +1,35 @@
-print('hello')
-#thi is my first git addition
+def encoded_password():
+    input_string = input("Please enter your password to encode:")
+    result_string = ""
+
+    for char in input_string:
+        if char.isdigit():
+            new_char = str((int(char) + 3) % 10)
+            result_string += new_char
+        else:
+            result_string += char
+    print("Encoded Password:", result_string)
+
+    def menu():
+        while True:
+            print("""
+    Menu
+    -------------
+    1. Encode
+    2. Decode
+    3. Quit
+                   """)
+            menu_option = int(input("Please enter an option: "))
+            if menu_option == 1:
+                encoded_password()
+
+            elif menu_option == 2:
+                decoded_password()
+            elif menu_option == 3:
+                print("Exiting the program.")
+                break
+            else:
+                print("Invalid option. Please choose 1, 2, or 3.")
+
+    if __name__ == "__main__":
+        menu()
