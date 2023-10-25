37d0d2c718f9ce85387c21ea01ee19f7792aa42d
EKS2003 <ekrivoysekler@ufl.edu>
2023-10-25 16:04:23 -0400

    Encoder Menu

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def encoded_password():
             result_string += new_char
         else:
             result_string += char
-    print("Encoded Password:", result_string)
+    return result_string
 
-    def menu():
+def menu():
         while True:
             print("""
     Menu
@@ -21,15 +21,15 @@ def encoded_password():
                    """)
             menu_option = int(input("Please enter an option: "))
             if menu_option == 1:
-                encoded_password()
+                print(encoded_password())
 
-            elif menu_option == 2:
-                decoded_password()
+            # elif menu_option == 2:
+            #     decoded_password()
             elif menu_option == 3:
                 print("Exiting the program.")
                 break
             else:
                 print("Invalid option. Please choose 1, 2, or 3.")
 
-    if __name__ == "__main__":
-        menu()
+if __name__ == "__main__":
+    menu()
